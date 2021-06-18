03be0e981c50af9be3d9ec47d7d778dc3153e4c7
Yoann <yoann.clement@epsi.fr>
2021-06-18 14:22:39 +0200

    YC - Dash aws users

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
-
+use Aws\Exception\AwsException;
+use Aws\Iam\IamClient;
+use Aws\S3\S3Client;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,9 +16,24 @@ class DashboardController extends AbstractController
      */
     public function index(): Response
     {
+        // The same options that can be provided to a specific client constructor can also be supplied to the Aws\Sdk class.
+        // Use the us-west-2 region and latest version of each client.
+        $sharedConfig = [
+            'region' => 'eu-west-2',
+            'version' => 'latest'
+        ];
+
+        // Create an SDK class used to share configuration across clients.
+        $sdk = new S3Client($sharedConfig);
+        $bucketName = 'espace-partage-epsi-i1-dev';
+
+        $result = $sdk->listObjects(['Bucket' => $bucketName]);
+
         return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController'
+            'controller_name' => 'DashboardController',
+            'file' => $result
         ]);
+
     }
 
     
