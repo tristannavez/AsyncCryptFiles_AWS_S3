0e27051030613e76316c029d66ef08dd12454df1
CLEMENT Yoann <yoannclement@labrosseetdupont.com>
2021-06-19 18:13:58 +0200

    YC - Commentaire

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -7,11 +7,9 @@ use Aws\S3\S3Client;
 use Crypt_GPG;
 use Crypt_GPG_BadPassphraseException;
 use Crypt_GPG_Exception;
-use Crypt_GPG_FileException;
 use Crypt_GPG_KeyNotFoundException;
 use Crypt_GPG_NoDataException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -24,29 +22,38 @@ class DashboardController extends AbstractController
      */
     public function index(): Response
     {
-
+        // Création d'un tableau contenant la configuration d'aws
         $sharedConfig = [
             'region' => 'eu-west-3',
             'version' => 'latest'
         ];
 
+        // Instanciation un objet S3 en passant en paramétre le tableau de configuration
         $s3 = new S3Client($sharedConfig);
+        // Création d'une variable contenant le nom du bucket
         $bucketName = 'espace-partage-epsi-i1-dev';
 
+        // Récupération des fichiers dans le bucket
         $files = $s3->listObjects(['Bucket' => $bucketName]);
 
+        // On créer un tableau
         $file = array();
 
+        // Parcours de l'ensemble des fichiers du bucket
         for($i=0;$i<count($files['Contents']);$i++){
+            // Changement du format de date du fichier
             $date = date('d-m-y',strtotime($files['Contents'][$i]['LastModified']));
+            // Ajout du titre et de la date dans notre tableau file
             $file[] = ([
                 'titre' => $files['Contents'][$i]['Key'],
                 'date' => $date
             ]);
         }
 
+        // Récupération des utilisateurs du site
         $users = $this->getDoctrine()->getRepository(User::class)->findAll();
 
+        // Envoie des données vers la page dashboard
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
             'file' => $file,
@@ -68,24 +75,25 @@ class DashboardController extends AbstractController
             'version' => 'latest'
         ];
         $s3 = new S3Client($sharedConfig);
-        
         $bucketName = 'espace-partage-epsi-i1-dev';
         
-        // Je récupère le fichier uploade
+        // Récupération du fichier envoyé
         $json_file = $request->files->get('fileToUpload');
-
+        // Récupération du nom du fichier envoyé
         $fichier = $json_file->getClientOriginalName();
+        // Récupération de l'url du fichier envoyé
         $url_fichier = $json_file->getPathName();
-
+        // Récupération du contenu du fichier envoyé
         $content_file = file_get_contents($url_fichier);
+        // Ajout du contenu précédemment récupéré signé et crypté
         file_put_contents($fichier, $this->contentEncryption($content_file));
-
+        // Envoi du fichier signé et crypté dans le bucket
         $s3->putObject([
             'Bucket' => $bucketName,
             'Key' => $fichier,
             'SourceFile' => $url_fichier
         ]);
-
+        // Redirection vers la page dashboard une fois l'envoi terminé
         return $this->redirectToRoute('dashboard');
 
     }
@@ -100,13 +108,12 @@ class DashboardController extends AbstractController
             'region' => 'eu-west-3',
             'version' => 'latest'
         ];
-
         $s3 = new S3Client($sharedConfig);
-
         $bucketName = 'espace-partage-epsi-i1-dev';
-        $file =  $request->get('file');
-        $saveAs = dirname(__DIR__).'FileBucket';
 
+        // Récupération du nom du fichier envoyé
+        $file =  $request->get('file');
+        // Récupération du fichier à télécharger dans le bucket
         $fileBucket = $s3->getObject([
             'Bucket' => $bucketName,
             'Key' => $file,
@@ -114,7 +121,7 @@ class DashboardController extends AbstractController
         ]);
 
         dd($fileBucket);
-
+        // Décryptage du fichier téléchargé
         //$decryptedFile = $this->fileDecryption($encryptedFile);
     }
 
@@ -128,14 +135,19 @@ class DashboardController extends AbstractController
      */
     public function contentEncryption($content)
     {
+        // Récupération de la clé public
         $publicKey = file_get_contents(dirname(__DIR__).'/keys/public.txt');
-        // ou récupération de la clé publique depuis un serveur AWS S3 par exemple.
+        // Instanciation d' l'objet Crypt GPG
         $gpg = new Crypt_GPG();
+        // Ajout de la clé récupéré précédemment dans l'objet
         $info = $gpg->importKey($publicKey);
+        // Ajout d'une signature dans l'objet via la clé public
         $gpg->addSignKey($info[ 'fingerprint' ]);
+        // Ajout de l'encryptage dans l'objet via la clé public
         $gpg->addEncryptKey($info[ 'fingerprint' ]);
+        // Encryptage et signature du contenu du fichier envoyé depuis la fonction d'upload
         $encryptedContent = $gpg->encryptAndSign($content);
-
+        // Retour du contenu encrypté et signé
         return $encryptedContent;
     }
 
@@ -149,12 +161,14 @@ class DashboardController extends AbstractController
      */
     public function contentDecryption($encryptedContent)
     {
+        // Récupération de la clé privée
         $privateKey = file_get_contents(dirname(__DIR__).'/keys/private.txt');
-        // ou récupération de la clé privée depuis un serveur AWS S3 par exemple.
         $gpg = new Crypt_GPG();
         $info = $gpg->importKey($privateKey);
         $gpg->addSignKey($info[ 'fingerprint' ]);
+        // Ajout de la clé de décryptage accompagné de la phrase secrète
         $gpg->addDecryptKey($info[ 'fingerprint' ], "decrypt");
+        // Decryptage et vérification du contenu du fichier envoyé depuis la fonction d'upload
         $decryptedContent = $gpg->decryptAndVerify($encryptedContent);
 
         dd($decryptedContent);
